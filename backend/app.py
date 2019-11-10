589338fe2ab9851c814b04d056f011ae63271ab9
nadavyer <nadavyerushalmi@gmail.com>
2019-11-10 11:06:54 +0200

    first commit

---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+from flask_cors import CORS
+
+app = Flask(__name__,
+            static_folder = "../frontend/dist/static",
+            template_folder = "../frontend/dist")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+@app.route('/api/shorten', methods=['POST'])
+def shorten_url():
+    print(request.data)
+    return jsonify('hello')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def index(path):
+    if app.debug:
+        return requests.get('http://localhost:8080/{}'.format(path)).text
+    return render_template("index.html")
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+console.log('Im running');
+
+const shortenLinkEndpoint = 'http://localhost:5000/api/shorten';
+const shortenLinkRequestOptions = {
+  method: 'post',
+};
+
+const handleShortenLinkResponse = (response) => {
+  console.log('response', response);
+  response.json().then((result) => {
+    console.log('result', result);
+  });
+};
+
+fetch(shortenLinkEndpoint, shortenLinkRequestOptions).then(handleShortenLinkResponse);
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
