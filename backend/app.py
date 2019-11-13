b40f50e62a43cc97c3eed858730f5c405bc715c1
nadavyer <nadavyerushalmi@gmail.com>
2019-11-13 12:25:30 +0200

    finished. need to add stats

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, jsonify, request, redirect, abort, session
+from flask_cors import CORS
+from random import choice
 from flask_sqlalchemy import SQLAlchemy
 import requests
-from flask_cors import CORS
 import string
-from random import choice
 import json
+import validators
+
 
 
 
@@ -15,6 +17,7 @@ app = Flask(__name__,
             template_folder = "../frontend/dist")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 
@@ -24,16 +27,13 @@ class Url(db.Model):
     code_url = db.Column(db.String(10), primary_key = True)
     long_url = db.Column(db.Text, nullable = False)
 
-    # def __init__(self, code_url: code_url, long_url: long_url **kwargs):
-    #     super().__init__(**kwargs)
-
 
 db.create_all()
 
 
 
 
-def gen_code_for_long_url(long_url):
+def gen_code_for_long_url(long_url): 
     chars = string.ascii_letters + string.digits
     length = 5
     code = ''.join(choice(chars) for x in range(length))
@@ -45,8 +45,14 @@ def gen_code_for_long_url(long_url):
 
 
 def validate_url(long_url):
-    return True
+    return validators.url(long_url)
 
+def prepare_for_url_validation(long_url):
+    long_url_7_prefix = long_url[:7]
+    long_url_8_prefix = long_url[:8]
+    if long_url_7_prefix == 'http://' or long_url_8_prefix == 'https://':
+        return long_url
+    return 'http://' + long_url
 
 
 
@@ -58,6 +64,9 @@ def index():
 def shorten_url():
     request_json = request.json
     long_url = request_json['longUrl']
+    print (long_url)
+    long_url = prepare_for_url_validation(long_url)
+    print (long_url)
     if not validate_url(long_url):
         return jsonify('Not a valid URL')
     code_url = gen_code_for_long_url(long_url)
@@ -68,25 +77,20 @@ def shorten_url():
 
 @app.route('/<short_url>')
 def get_long_url(short_url):
-    # code = short_url[21:]
-    print (short_url)
     exist = db.session.query(
         db.exists().where(Url.code_url == short_url)).scalar()
     if not exist:
         return  abort(404)
 
     ret  = db.session.query(Url).filter_by(code_url = short_url).first().long_url
-    print (ret)
-    if ret[:5] == "http" or ret[:6] == "https":
-        pass 
-    else:
-        ret = "http://" +  ret 
-    # ret = ret if ret[:5] == "http" else f"http://{db[short_url]}"
     return redirect(ret, code=302)
+
+
+@app.route('')
 
 
 
 
 if __name__ == '__main__':
-    app.run(debug= True, port= '5000')
+    app.run(debug= True, port = '5000')
         
